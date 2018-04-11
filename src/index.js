572f21ec6691fe5610ffbd895865080d7bbad112
Teddarific <teddy.ni.19@dartmouth.edu>
2018-04-11 15:55:52 -0400

    short assignment

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("yes");
+import $ from 'jquery';
+import './style.scss';
+
+let num = 0;
+setInterval(() => {
+  num += 1;
+  $('#main').text(`You've been on this page for ${num} seconds.`);
+}, 1000);
