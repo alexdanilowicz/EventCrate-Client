ef43e8e2459591e6a27f900473d6f939503cf5e9
Teddarific <teddy.ni.19@dartmouth.edu>
2018-04-26 13:46:09 -0400

    routing basics

--- src/index.js
+++ src/index.js
@@ -1,8 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, NavLink, Switch } from 'react-router-dom';
 
-const App = () => {
-  return <div className="test">All the React are belong to us!</div>;
+
+const About = (props) => {
+  return <div> All there is to know about me </div>;
 };
+
+const Welcome = (props) => {
+  return <div>Welcome</div>;
+};
+
+const Test = (props) => {
+  return <div> ID: {props.match.params.id} </div>;
+};
+
+const FallBack = (props) => {
+  return <div>URL Not Found</div>;
+};
+
+const App = (props) => {
+  return (
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Welcome} />
+          <Route path="/about" component={About} />
+          <Route exact path="/test/:id" component={Test} />
+          <Route component={FallBack} />
+        </Switch>
+      </div>
+    </Router>
+  );
+};
+
+const Nav = (props) => {
+  return (
+    <nav>
+      <ul>
+        <li><NavLink to="/">Home</NavLink></li>
+        <li><NavLink to="/about">About</NavLink></li>
+        <li><NavLink to="/test/id1">test id1</NavLink></li>
+        <li><NavLink to="/test/id2">test id2</NavLink></li>
+      </ul>
+    </nav>
+  );
+};
+
 
 ReactDOM.render(<App />, document.getElementById('main'));
