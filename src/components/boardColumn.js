387fb1826282304a5b76d2ba9c51d9e2c1eba14b
Alexander Danilowicz <alexander.j.danilowicz.19@dartmouth.edu>
2018-10-06 16:52:28 -0400

    stlye

--- src/components/boardColumn.js
+++ src/components/boardColumn.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { Card } from 'semantic-ui-react';
 import EventCard from '../components/event-card';
 
+const getRandomColor = () => {
+  const colorValues = ['red', 'blue', 'green', 'yellow', 'teal', 'violet', 'orange', 'purple', 'pink'];
+  return colorValues[Math.floor(Math.random() * colorValues.length)];
+};
+
 const BoardColumn = (props) => {
   const cards = props.events.map(eventInfo => (
     <Card.Group key={eventInfo.id}>
@@ -12,15 +17,17 @@ const BoardColumn = (props) => {
         eventLocation={eventInfo.location}
         eventClubName={eventInfo.clubName}
         eventContent={eventInfo.description}
-        eventTagColor="green"
-        eventTags="CS, Hackathon"
+        eventTagColor={`${getRandomColor()}`}
+        eventTags={`${eventInfo.clubName}, ${eventInfo.name}`}
       />
     </Card.Group>
   ));
 
   return (
     <div className="board-column">
-      <div className="column-title"> {props.title} </div>
+      <h3 className="ui header">
+        {props.title}
+      </h3>
       <div className="column-items">
         {cards}
       </div>
